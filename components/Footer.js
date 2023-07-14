657f80afae8c92e2a7783ef6ed57a6322087b225
Kevin Lam <mtn013209@gmail.com>
2023-07-13 23:37:15 -0700

    ready to fill in

--- components/Footer.js
+++ components/Footer.js
@@ -1,3 +1,7 @@
+import Image from "next/image"
+import extLink from "@/public/extLink.svg"
+import github from "@/public/github.svg"
+
 export default function Footer() {
   return (
     <footer className="footer pb-5" id="footer">
@@ -8,6 +12,24 @@ export default function Footer() {
           <a href="https://brittanychiang.com/">Brittany Chiang</a>. Icons{" "}
           <a>attribution</a>.
         </p>
+        <div className="pt-0 pb-0">
+          <a className="px-2" href="">
+            <Image
+              src={extLink}
+              width={25}
+              height={25}
+              alt="external link icon"
+            />
+          </a>
+          <a className="px-2" href="">
+            <Image
+              src={github}
+              width={25}
+              height={25}
+              alt="external link icon"
+            />
+          </a>
+        </div>
       </div>
     </footer>
   )
